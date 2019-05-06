89a6e41e9f57b81eb25b20e3283d647a64dbaa3d
Keren Sun <35299472+KerenSUN@users.noreply.github.com>
2019-05-05 23:27:21 -0400

    Add files via upload

--- annotated_data/unspecified_generator.py
+++ annotated_data/unspecified_generator.py
@@ -1,7 +1,8 @@
 import os
 
-for root, dirs, files in os.walk(os.getcwd()):
+for root, dirs, files in os.walk('annotated_data'):
     for name in files:
+        print(name)
         if '.txt' in name:
             with open(os.path.join(root, name)) as f:
                 lst = list()
@@ -14,7 +15,7 @@ for root, dirs, files in os.walk(os.getcwd()):
                         text = tokens[1]
                         start = tokens[2]
                         end = tokens[3]
-                        lst.append([text, start, end, tag])
+                        lst.append([int(text), int(start), int(end), tag])
                     if tokens[0] == 'TLINK':
                         tag1 = tokens[1]
                         text = tokens[2]
@@ -23,21 +24,19 @@ for root, dirs, files in os.walk(os.getcwd()):
                         tag2 = tokens[5]
                         start2 = tokens[7]
                         end2 = tokens[8]
-                        relations.append([text, start1, start2])
-            f_write = open(name, 'a+')
-            lst.sort()
-            pre = None
-            for cur in lst:
-                if pre is None:
+                        relations.append([int(text), int(start1), int(start2)])
+                f_write = open(name, 'w')
+                lst.sort()
+                pre = None
+                for cur in lst:
+                    if pre is None:
+                        pre = cur
+                        continue
+                    if pre[0] != cur[0]:
+                        pre = cur
+                        continue
+                    if [pre[0], pre[1], cur[1]] not in relations:
+                        line_unspecified = '\t'.join(['TLINK', pre[3], str(pre[0]), str(pre[1]), str(pre[2]), cur[3], str(cur[0]), str(cur[1]), str(cur[2]), 'unspecified'])
+                        f_write.write(line_unspecified)
+                        f_write.write('\n')
                     pre = cur
-                    continue
-                if pre[0] != cur[0]:
-                    pre = cur
-                    continue
-                if [pre[0], pre[1], cur[1]] not in relations:
-                    line_unspecified = '\t'.join(['TLINK', pre[3], pre[0],
-                                                  pre[1], pre[2], cur[3],
-                                                  cur[0], cur[1], cur[2],
-                                                  'unspecified'])
-                    f_write.write(line_unspecified)
-                    f_write.write('\n')
